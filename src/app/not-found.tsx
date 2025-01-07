0e816691deff20b13a450ed398dfd3af51a7dd33
Julia Finocchiaro <jgfino.dev@gmail.com>
2025-01-07 01:40:28 -0500

    more not found fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,6 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+export const dynamic = "force-dynamic";
+import { redirect } from "next/navigation";
 
 export default function NotFound() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/");
-  }, [router]);
-
-  return null;
+  redirect("/");
 }
