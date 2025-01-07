ec40092b042d04067ccd4c1a3209917614a53314
Julia Finocchiaro <jgfino.dev@gmail.com>
2025-01-07 01:59:26 -0500

    comment out about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,166 +1,167 @@
-import Image from "next/image";
-import julia from "../../../public/julia.jpg";
+// import Image from "next/image";
+// import julia from "../../../public/julia.jpg";
 
 export default async function About() {
   return (
-    <div className="p-8 flex flex-col items-center h-full lg:justify-center">
-      <div className="flex flex-col gap-8" style={{ maxWidth: 1000 }}>
-        <div className="flex flex-col lg:flex-row gap-8 items-center">
-          <Image
-            src={julia}
-            alt="Julia Finocchiaro"
-            height={600}
-            style={{ objectFit: "contain" }}
-          />
-          <div>
-            <p>
-              Hi! I&apos;m Julia, a concert, portrait, and event photographer.
-              Combining my love of live music and photography has been a dream
-              come true, and I am so grateful to work with so many talented
-              artists and creatives. At the end of the day I&apos;m still a fan
-              first, and I do my best to use this to my advantage to capture the
-              full experience and energy of a live performance.
-            </p>
-            <br />
-            <p>
-              Editing is my favorite part of the photography process, and while
-              I&apos;ve worked hard to perfect my own bright and colorful style,
-              I also enjoy taking a creative vision and bringing it to life, so
-              send me your inspo, Pinterest boards, or color palettes and
-              I&apos;ll do my best to match what you&apos;re looking for. I am
-              also available for photo editing services.
-            </p>
-            <br />
-            <p>
-              Previous clients include <b>Tessa Violet</b>, <b>The Kid LAROI</b>
-              , <b>Rise Against</b>,{" "}
-              <b>Foundations Management (Zolita, The Brook and the Bluff)</b>,{" "}
-              <b>We Three</b>, <b>Solence</b>, <b>Girli</b> and more. She has
-              photographed artists at venues from small local clubs to arenas
-              and stadiums, and have shot at major festivals including{" "}
-              <b>Gov Ball</b>, <b>Boston Calling</b>, and{" "}
-              <b>Head in the Clouds</b>
-            </p>
-            <br />
-            <p>
-              She has had her work featured in various publications, including{" "}
-              <a
-                href="https://www.instagram.com/unclearmag/?hl=en"
-                className="underline"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <b>Unclear Magazine</b>
-              </a>
-              , <b>New England Sounds</b>, <b>The Huntington News</b>,{" "}
-              <b>ECHO Magazine</b>,{" "}
-              <a
-                href="https://www.instagram.com/thephotoladies/"
-                className="underline"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <b>The Photo Ladies</b>
-              </a>
-              ,{" "}
-              <a
-                href="https://respectyouryoungers.com/author/julia/"
-                className="underline"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <b>Respect Your Youngers</b>
-              </a>
-              , and many more. She served as photo editor for Northeastern
-              University&apos;s{" "}
-              <a
-                href="https://www.tastemakersmag.com/photos?author=6179a643aedc4374d5fd60a8"
-                className="underline"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {" "}
-                <b>Tastemakers Magazine</b>{" "}
-              </a>
-              for two years while she was a student there.
-            </p>
-            <br />
-            <p>
-              <b>Selected Written Work:</b>
-            </p>
-            <br />
-            <ul>
-              <li>
-                <a
-                  href="https://huntnewsnu.com/78040/lifestyle/reviews/bad-bunny-returns-to-boston-with-the-most-wanted-show/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline"
-                >
-                  Show Review: Bad Bunny&apos;s Most Wanted Tour{" "}
-                  <i>(The Huntington News)</i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://respectyouryoungers.com/reviews/noah-kahan-took-89-to-boston-and-sold-out-fenway-park/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline"
-                >
-                  Show Review: Noah Kahan @ Fenway Park{" "}
-                  <i>(Respect Your Youngers)</i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.tastemakersmag.com/show-reviews/taylor-swift-gillette"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline"
-                >
-                  Show Review: Taylor Swift&apos;s The Eras Tour{" "}
-                  <i>(Tastemakers Magazine)</i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://newenglandsounds.com/2023/08/30/live-review-photos-bruce-springsteen-and-the-e-street-band-08-26-23/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline"
-                >
-                  Show Review: Bruce Springsteen @ Gillette Stadium{" "}
-                  <i>(New England Sounds)</i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://respectyouryoungers.com/reviews/valley-boston-2024/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline"
-                >
-                  Show Review: Valley @ House of Blues{" "}
-                  <i>(Respect Your Youngers)</i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <p>
-          Julia is available for while based primarily in <b>Boston, MA</b> and{" "}
-          <b>NYC</b>, she is also available for travel.
-        </p>
-        <a
-          href="mailto:julia@jfino.photo.com"
-          className="hover:underline self-center"
-        >
-          <p className="text-xl font-semibold mb-16 lg:mb-0">
-            julia@jfinophoto.com
-          </p>
-        </a>
-      </div>
-    </div>
+    <></>
+    // <div className="p-8 flex flex-col items-center h-full lg:justify-center">
+    //   <div className="flex flex-col gap-8" style={{ maxWidth: 1000 }}>
+    //     <div className="flex flex-col lg:flex-row gap-8 items-center">
+    //       <Image
+    //         src={julia}
+    //         alt="Julia Finocchiaro"
+    //         height={600}
+    //         style={{ objectFit: "contain" }}
+    //       />
+    //       <div>
+    //         <p>
+    //           Hi! I&apos;m Julia, a concert, portrait, and event photographer.
+    //           Combining my love of live music and photography has been a dream
+    //           come true, and I am so grateful to work with so many talented
+    //           artists and creatives. At the end of the day I&apos;m still a fan
+    //           first, and I do my best to use this to my advantage to capture the
+    //           full experience and energy of a live performance.
+    //         </p>
+    //         <br />
+    //         <p>
+    //           Editing is my favorite part of the photography process, and while
+    //           I&apos;ve worked hard to perfect my own bright and colorful style,
+    //           I also enjoy taking a creative vision and bringing it to life, so
+    //           send me your inspo, Pinterest boards, or color palettes and
+    //           I&apos;ll do my best to match what you&apos;re looking for. I am
+    //           also available for photo editing services.
+    //         </p>
+    //         <br />
+    //         <p>
+    //           Previous clients include <b>Tessa Violet</b>, <b>The Kid LAROI</b>
+    //           , <b>Rise Against</b>,{" "}
+    //           <b>Foundations Management (Zolita, The Brook and the Bluff)</b>,{" "}
+    //           <b>We Three</b>, <b>Solence</b>, <b>Girli</b> and more. She has
+    //           photographed artists at venues from small local clubs to arenas
+    //           and stadiums, and have shot at major festivals including{" "}
+    //           <b>Gov Ball</b>, <b>Boston Calling</b>, and{" "}
+    //           <b>Head in the Clouds</b>
+    //         </p>
+    //         <br />
+    //         <p>
+    //           She has had her work featured in various publications, including{" "}
+    //           <a
+    //             href="https://www.instagram.com/unclearmag/?hl=en"
+    //             className="underline"
+    //             target="_blank"
+    //             rel="noreferrer"
+    //           >
+    //             <b>Unclear Magazine</b>
+    //           </a>
+    //           , <b>New England Sounds</b>, <b>The Huntington News</b>,{" "}
+    //           <b>ECHO Magazine</b>,{" "}
+    //           <a
+    //             href="https://www.instagram.com/thephotoladies/"
+    //             className="underline"
+    //             target="_blank"
+    //             rel="noreferrer"
+    //           >
+    //             <b>The Photo Ladies</b>
+    //           </a>
+    //           ,{" "}
+    //           <a
+    //             href="https://respectyouryoungers.com/author/julia/"
+    //             className="underline"
+    //             target="_blank"
+    //             rel="noreferrer"
+    //           >
+    //             <b>Respect Your Youngers</b>
+    //           </a>
+    //           , and many more. She served as photo editor for Northeastern
+    //           University&apos;s{" "}
+    //           <a
+    //             href="https://www.tastemakersmag.com/photos?author=6179a643aedc4374d5fd60a8"
+    //             className="underline"
+    //             target="_blank"
+    //             rel="noreferrer"
+    //           >
+    //             {" "}
+    //             <b>Tastemakers Magazine</b>{" "}
+    //           </a>
+    //           for two years while she was a student there.
+    //         </p>
+    //         <br />
+    //         <p>
+    //           <b>Selected Written Work:</b>
+    //         </p>
+    //         <br />
+    //         <ul>
+    //           <li>
+    //             <a
+    //               href="https://huntnewsnu.com/78040/lifestyle/reviews/bad-bunny-returns-to-boston-with-the-most-wanted-show/"
+    //               target="_blank"
+    //               rel="noreferrer"
+    //               className="underline"
+    //             >
+    //               Show Review: Bad Bunny&apos;s Most Wanted Tour{" "}
+    //               <i>(The Huntington News)</i>
+    //             </a>
+    //           </li>
+    //           <li>
+    //             <a
+    //               href="http://respectyouryoungers.com/reviews/noah-kahan-took-89-to-boston-and-sold-out-fenway-park/"
+    //               target="_blank"
+    //               rel="noreferrer"
+    //               className="underline"
+    //             >
+    //               Show Review: Noah Kahan @ Fenway Park{" "}
+    //               <i>(Respect Your Youngers)</i>
+    //             </a>
+    //           </li>
+    //           <li>
+    //             <a
+    //               href="https://www.tastemakersmag.com/show-reviews/taylor-swift-gillette"
+    //               target="_blank"
+    //               rel="noreferrer"
+    //               className="underline"
+    //             >
+    //               Show Review: Taylor Swift&apos;s The Eras Tour{" "}
+    //               <i>(Tastemakers Magazine)</i>
+    //             </a>
+    //           </li>
+    //           <li>
+    //             <a
+    //               href="https://newenglandsounds.com/2023/08/30/live-review-photos-bruce-springsteen-and-the-e-street-band-08-26-23/"
+    //               target="_blank"
+    //               rel="noreferrer"
+    //               className="underline"
+    //             >
+    //               Show Review: Bruce Springsteen @ Gillette Stadium{" "}
+    //               <i>(New England Sounds)</i>
+    //             </a>
+    //           </li>
+    //           <li>
+    //             <a
+    //               href="https://respectyouryoungers.com/reviews/valley-boston-2024/"
+    //               target="_blank"
+    //               rel="noreferrer"
+    //               className="underline"
+    //             >
+    //               Show Review: Valley @ House of Blues{" "}
+    //               <i>(Respect Your Youngers)</i>
+    //             </a>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //     </div>
+    //     <p>
+    //       Julia is available for while based primarily in <b>Boston, MA</b> and{" "}
+    //       <b>NYC</b>, she is also available for travel.
+    //     </p>
+    //     <a
+    //       href="mailto:julia@jfino.photo.com"
+    //       className="hover:underline self-center"
+    //     >
+    //       <p className="text-xl font-semibold mb-16 lg:mb-0">
+    //         julia@jfinophoto.com
+    //       </p>
+    //     </a>
+    //   </div>
+    // </div>
   );
 }
